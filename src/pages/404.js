88d41ab463697fbb449000abf379b3cf1acab4f7
liuyingjie1130 <1410505543@qq.com>
2020-02-23 22:26:07 +0800

    test

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,7 +2,7 @@ export default function ({dispatch}){
     return (
       <div style={{display: 'flex', justifyContent:'center'}}>
         <div style={{display: 'flex', flexDirection:'column', marginTop:60, alignItems:'center'}}>
-        <span style={{marginTop: 30, color: '#333', fontSize:16}}>抱歉，您访问的页面不存在</span>
+        <span style={{marginTop: 30, color: '#333', fontSize:16}}>抱歉，您访问的页面不存在不存在不存在</span>
         </div>
       </div>
     )
